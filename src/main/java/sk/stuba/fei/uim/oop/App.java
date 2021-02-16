187ed7df8e43e79eebd31eb32f9339fef1b322cd
Beny <xlavrincikb>
2021-02-16 17:15:21 +0100

    typy

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -3,5 +3,17 @@ package sk.stuba.fei.uim.oop;
 public class App {
     public static void main(String[] argumentyZKonzoly){
         System.out.println("Hello world!");
+
+        int celeCislo = 5;
+        long celeCisloVacsie = 5L;
+        float desatinneSinglePrecision = 5.5f;
+        double desatinneDoublePrescision = 5.5;
+        char znak = 'a';
+        String retazec = "abc";
+
+
+
+
+
     }
 }
